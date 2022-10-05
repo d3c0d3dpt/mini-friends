21705d4761a1cfc0aee27b2bbe920a359ee13775
Alvaro Ferreira <aferrira@dunelm.com>
2022-10-05 13:36:46 +0100

    👷 send build information to cypress dashboard

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "cypress";
 import { initPlugin } from "cypress-plugin-snapshots/plugin";
 
 export default defineConfig({
+  projectId: "gpcfsf",
+
   e2e: {
     setupNodeEvents(on, config) {
       initPlugin(on, config);
