0990e1978e6f1fa553b2f0329ddbee8fd6854e03
Alvaro Ferreira <aferrira@dunelm.com>
2022-10-05 14:36:50 +0100

    👷 test artifact upload

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "cypress";
 
 import { initPlugin } from "cypress-plugin-snapshots/plugin";
 
+const viewportWidth = 440;
+const viewportHeight = 900;
+
 export default defineConfig({
   projectId: "gpcfsf",
 
@@ -9,12 +12,28 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       initPlugin(on, config);
 
+      on("before:browser:launch", (browser, launchOptions) => {
+        if (browser.name === "electron" && browser.isHeadless) {
+          launchOptions.preferences.width = viewportWidth;
+          launchOptions.preferences.height = viewportHeight;
+          launchOptions.preferences.resizable = false;
+          return launchOptions;
+        }
+
+        if (browser.name === "chrome" && browser.isHeadless) {
+          launchOptions.args.push(
+            "--window-size=" + viewportWidth + "," + viewportHeight
+          );
+          return launchOptions;
+        }
+      });
+
       return config;
     },
   },
 
-  viewportHeight: 900,
-  viewportWidth: 1440,
+  viewportWidth,
+  viewportHeight,
 
   video: false,
 });
